d6cf0e4eb7e9896c3abc43ac4673ed195760debc
dbiegan <dbiegan@asu.edu>
2022-02-10 22:36:50 -0700

    Created options menu

--- RoverSim/Assets/Scripts/MainMenuController.cs
+++ RoverSim/Assets/Scripts/MainMenuController.cs
@@ -5,11 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
     // Handle the event when clicking the Start button
     public void clickStartButton()
     {
         Debug.Log("Starting the simulation...");
         SceneManager.LoadScene("NavigationScene");
+    }
+
+    // Handle the event when clicking the Options button
+    public void clickOptionsButton()
+    {
+        optionsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    // Handle the event when clicking the Back button
+    public void clickBackButton()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
     }
 
     // Handle the event when clicking the Quit button
