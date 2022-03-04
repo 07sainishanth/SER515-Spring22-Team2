7885476ef3ddee402dd52e4c376265df102e5ad5
alekhya chepyala <achepya2@asu.edu>
2022-03-03 18:37:20 -0700

    TG-83 Create a platform us-38 task 83

---
+++ Random point/Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    int wholeNumber = 3;
+    float decimalNumber = 3.45f;
+    // Start is called before the first frame update
+    void Start()
+    {
+     rb = GetComponent<Rigidbody>();
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizantalInput = Input.GetAxis("Horizantal");
+         float verticallInput = Input.GetAxis("Vertical");
+         rb.velocity = new Vector3(horizantalInput * 5f,rb.velocity.y, verticallInput * 5f);
+        
+         if (Input.GetButtonDown("Jump"))
+         {
+             rb.velocity = new Vector3(rb.velocity.x, 5f, rb.velocity.z);
+         }
+    }
+}
