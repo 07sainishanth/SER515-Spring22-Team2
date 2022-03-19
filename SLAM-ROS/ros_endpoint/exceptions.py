739adb8baabbdb268df3c7c5d1bf88485c5fa7b4
Sai Nishanth Vaka <vsainishanth@gmail.com>
2022-03-18 19:58:33 -0700

    ENvironent setup ros endpoint integration interface

---
+++ SLAM-ROS/ros_endpoint/exceptions.py
@@ -0,0 +1,13 @@
+## Sai Nishanth Vaka
+
+
+
+import glob
+import os
+
+class ExceptionHandlerROS(object):
+    """docstring for ExceptionHandlerROS"""
+    def __init__(self, arg):
+        super(ExceptionHandlerROS, self).__init__()
+        self.arg = arg
+        
---
+++ SLAM-ROS/ros_endpoint/main.py
@@ -0,0 +1,15 @@
+## Sai Nishanth Vaka
+
+
+
+import glob
+import os
+
+from setuptools import setup
+
+package_name = 'ROS Endpoint'
+
+
+
+if __name__ == '__main__':
+    a = 2
