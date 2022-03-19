02284079a79ab50e385c5ddfd74ec2d54b3ece43
Sai Nishanth Vaka <vsainishanth@gmail.com>
2022-03-18 19:15:13 -0700

    launch environment for  RVIZ and unity integration

--- SLAM-ROS/unity_integration/main.py
+++ SLAM-ROS/unity_integration/main.py
@@ -7,26 +7,22 @@ import os
 
 from setuptools import setup
 
-package_name = 'unity_slam_example'
+package_name = 'unity_intergration'
 
 setup(
     name=package_name,
     version='0.0.1',
     packages=[],
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+
         (os.path.join('share', package_name), [
-                                                'rviz/nav2_unity.rviz',
-                                                'launch/unity_slam_example.py',
-                                                'launch/unity_viz_example.py'
+                                                # 'rviz/nav.rviz',
+                                                'launch/run.py',
+                                                'launch/visual.py'
                                                 ])
     ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='Unity Robotics',
-    maintainer_email='unity-robotics@unity3d.com',
-    description='Unity Robotics Nav2 SLAM Example',
-    license='Apache 2.0',
-    tests_require=['pytest']
+    install_requires=['setuptools']
 )
+
+if __name__ == '__main__':
+    a = 2
