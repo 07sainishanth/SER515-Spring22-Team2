ef2dc44c4d85c1641ca352edb8d2f25549dd6288
David Bieganski <david.bieganski@asu.edu>
2022-04-08 00:14:02 -0700

    TG-153 #closed TG-169 #closed Added user controls to rover and created the pyramid destroyer rover :D

--- RoverSim/Assets/Scripts/MovementController.cs
+++ RoverSim/Assets/Scripts/MovementController.cs
@@ -11,27 +11,76 @@ public class MovementController : MonoBehaviour
 {
     public GameObject mainCamera;
     public GameObject roverCamera;
+    public GameObject cameraRig;
+    public GameObject rover;
+
+    Rigidbody roverRigidbody;
+    bool destroy;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        roverRigidbody = rover.GetComponent<Rigidbody>();
+        destroy = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F)) // Switch cameras by pressing the 'F' Key
+
+        if (!destroy)
         {
-            if (mainCamera.activeInHierarchy)
+            cameraRig.transform.position = rover.transform.position;
+
+            if (Input.GetKeyDown(KeyCode.F)) // Switch cameras by pressing the 'F' Key
             {
-                roverCamera.SetActive(true);
-                mainCamera.SetActive(false);
+                if (mainCamera.activeInHierarchy)
+                {
+                    roverCamera.SetActive(true);
+                    mainCamera.SetActive(false);
+                }
+                else
+                {
+                    roverCamera.SetActive(false);
+                    mainCamera.SetActive(true);
+                }
             }
-            else
+
+            if (roverCamera.activeInHierarchy) // Only move rover when rover Camera is active
             {
-                roverCamera.SetActive(false);
-                mainCamera.SetActive(true);
+                if (Input.GetKey(KeyCode.A)) // Rotate the camera counterclockwise
+                {
+                    cameraRig.transform.RotateAround(cameraRig.transform.position, Vector3.up, -45 * Time.deltaTime);
+                }
+
+                if (Input.GetKey(KeyCode.D)) // Rotate camera clockwise
+                {
+                    cameraRig.transform.RotateAround(cameraRig.transform.position, Vector3.up, 45 * Time.deltaTime);
+                }
+
+                if (Input.GetKey(KeyCode.W)) // Move forward
+                {
+                    rover.transform.Translate(10 * cameraRig.transform.forward * Time.deltaTime, Space.World);
+                }
+
+                if (Input.GetKey(KeyCode.S)) // Move backward
+                {
+                    rover.transform.Translate(-10 * cameraRig.transform.forward * Time.deltaTime, Space.World);
+                }
+
+                if (Input.GetKeyDown(KeyCode.Space) && rover.transform.localPosition.y <= 1.5) // Thrust upwards from ground
+                {
+                    roverRigidbody.AddForce(cameraRig.transform.up * 16000.0f, ForceMode.Impulse);
+                }
+
+                if (Input.GetKeyDown(KeyCode.X)) // Detonate pyramid destroyer :D
+                {
+                    rover.transform.localScale *= 25.0f;
+                    Destroy(rover, 0.05f);
+                    destroy = true;
+                    roverCamera.SetActive(false);
+                    mainCamera.SetActive(true);
+                }
             }
         }
     }
